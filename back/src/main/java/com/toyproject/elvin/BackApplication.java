0fc05c7402f761fc3d6d7e4da9837b25b89b1f43
shintwl <shinilheo0205@gmail.com>
2022-03-23 01:14:43 +0900

    aws s3 test

--- back/src/main/java/com/toyproject/elvin/BackApplication.java
+++ back/src/main/java/com/toyproject/elvin/BackApplication.java
@@ -3,11 +3,24 @@ package com.toyproject.elvin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+
 @SpringBootApplication
 public class BackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
+
+		Region region = Region.AWS_GLOBAL;
+		S3Client s3 = S3Client.builder().region(region).build();
+
+		// List buckets
+		ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
+		ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
+		listBucketsResponse.buckets().stream().forEach(x -> System.out.println(x.name()));
 	}
 
 }
